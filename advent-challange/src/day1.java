bca9c44738251ff05fea80f2d0fdfd101a515092
takacsagi95 <takacsagi95@gmail.com>
2022-12-02 10:59:27 +0100

    Advent coding part two

--- advent-challange/src/day1.java
+++ advent-challange/src/day1.java
@@ -3,27 +3,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class day1 {
+
   public static void main(String[] args) {
     //System.out.println(hashMapFromFile("/src/inputs/day1_input.txt"));
-    System.out.println(caloryCounting("src/input1.txt"));
+    //System.out.println(caloryCounting("src/input1.txt"));
+    System.out.println(topThreeCaloriesSum("src/input1.txt"));
   }
 
-  public static int caloryCounting(String filePath) {
+  public static int calorieCounting(String filePath) {
     HashMap<Integer, Integer> elvesAndCalories = hashMapFromFile(filePath);
     int maxCalories = 0;
-    for (Map.Entry<Integer, Integer> entry : elvesAndCalories.entrySet())
+    for (Map.Entry<Integer, Integer> entry : elvesAndCalories.entrySet()) {
       if (entry.getValue() > maxCalories) {
         maxCalories = entry.getValue();
       }
+    }
     return maxCalories;
   }
+
   // ^\n|^\s+\n
-  public static HashMap<Integer, Integer> hashMapFromFile(String filepath){
+  public static HashMap<Integer, Integer> hashMapFromFile(String filepath) {
     HashMap<Integer, Integer> map = new HashMap<>();
     try {
       Path path = Paths.get(filepath);
@@ -43,12 +48,12 @@ public class day1 {
       int allLinesSum = 0;
       Integer elf = 1;
       for (int i = 0; i < numLines.size(); i++) {
-        if (0 == numLines.get(i)){
+        if (0 == numLines.get(i)) {
           map.put(elf, allLinesSum);
           elf++;
           allLinesSum = 0;
         }
-        if (0 != numLines.get(i)){
+        if (0 != numLines.get(i)) {
           allLinesSum = allLinesSum + numLines.get(i);
         }
       }
@@ -56,5 +61,40 @@ public class day1 {
       throw new RuntimeException(e);
     }
     return map;
+  }
+
+  public static int topThreeCaloriesSum(String filePath) {
+    // Find how many calories delivered by the top three elves who carried most calories:
+    HashMap<Integer, Integer> elvesCalories = hashMapFromFile(filePath);
+    ArrayList<Integer> sortedCalorieSums = sortTheMapByValues(elvesCalories);
+    Integer[] topThree = new Integer[]{0, 0, 0};
+    int sumOfTopThree = 0;
+
+    for (int i = 0; i < topThree.length; i++) {
+      for (int j = sortedCalorieSums.size() - 1 - i; 0 < j; j--) {
+        if (sortedCalorieSums.get(j) > topThree[i]) {
+          topThree[i] = sortedCalorieSums.get(j);
+        }
+      }
+    }
+
+    for (int i = 0; i < topThree.length; i++) {
+      sumOfTopThree = sumOfTopThree + topThree[i];
+    }
+
+    return sumOfTopThree;
+  }
+
+  public static ArrayList<Integer> sortTheMapByValues(HashMap<Integer, Integer> map) {
+    ArrayList<Integer> list = new ArrayList<>();
+    HashMap<Integer, Integer> sortedMap = new HashMap<>();
+
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      list.add(entry.getValue());
+    }
+
+    Collections.sort(list);
+
+    return list;
   }
 }
