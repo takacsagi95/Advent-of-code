eec5b3d3a62581470b4694c1af8d96ea33c45cb1
takacsagi95 <takacsagi95@gmail.com>
2022-12-02 08:57:57 +0100

    Advent coding part one

---
+++ advent-challange/src/day1.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class day1 {
+  public static void main(String[] args) {
+    //System.out.println(hashMapFromFile("/src/inputs/day1_input.txt"));
+    System.out.println(caloryCounting("src/input1.txt"));
+  }
+
+  public static int caloryCounting(String filePath) {
+    HashMap<Integer, Integer> elvesAndCalories = hashMapFromFile(filePath);
+    int maxCalories = 0;
+    for (Map.Entry<Integer, Integer> entry : elvesAndCalories.entrySet())
+      if (entry.getValue() > maxCalories) {
+        maxCalories = entry.getValue();
+      }
+    return maxCalories;
+  }
+  // ^\n|^\s+\n
+  public static HashMap<Integer, Integer> hashMapFromFile(String filepath){
+    HashMap<Integer, Integer> map = new HashMap<>();
+    try {
+      Path path = Paths.get(filepath);
+      List<String> lines = Files.readAllLines(path);
+      List<Integer> numLines = new ArrayList<>();
+
+      for (int i = 0; i < lines.size(); i++) {
+
+        if (lines.get(i).isEmpty()) {
+          numLines.add(0);
+        } else {
+          Integer temp = Integer.valueOf(lines.get(i));
+          numLines.add(temp);
+        }
+
+      }
+      int allLinesSum = 0;
+      Integer elf = 1;
+      for (int i = 0; i < numLines.size(); i++) {
+        if (0 == numLines.get(i)){
+          map.put(elf, allLinesSum);
+          elf++;
+          allLinesSum = 0;
+        }
+        if (0 != numLines.get(i)){
+          allLinesSum = allLinesSum + numLines.get(i);
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return map;
+  }
+}
